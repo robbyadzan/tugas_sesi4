e91251b0376b4f386f774d41d1403bef5a8b8f44
robbyadzan <robby.adzan_ti24@nusaputra.ac.id>
2024-10-19 20:39:42 +0700

    Create Robby_Deret1.java

---
+++ Robby_Deret1.java
@@ -0,0 +1,13 @@
+public class Robby_Deret1 {
+    public static void main(String[] args) {
+       
+        for (int i = 0; i < 3; i++) {
+            
+            for (int j = 1; j <= 6; j++) {
+                System.out.print(j + " ");
+            }
+           
+            System.out.println();
+        }
+    }
+}
